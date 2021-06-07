66cd05f9e993b8307d689fd26e05bf4bee453f30
Ashutosh Jha <73036341+jhaashutosh@users.noreply.github.com>
2021-06-07 15:43:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ function stop()
 
 }
 
+var c=false;
+document.getElementById("startbtn").addEventListener("click",counter);
+
+function counter()
+{
+    c=true;
+}
+
 var counter=0;
 function jump(){
     if(character.classList != "animate")
@@ -20,19 +28,32 @@ function jump(){
     },300);
 }
 var checkDead = setInterval(function() {
+
+    
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+    
+    
+        if(blockLeft<20 && blockLeft>0 && characterTop>=130){
         //block.style.animation = "none";
-        alert("Game Over. score: "+ Math.floor(counter/100) );
+        alert("Game Over. score: "+ Math.floor(counter/100));
+        
+        c=0;
+        document.getElementById("scoreSpan").innerHTML = 0;   
         console.log(stop());
         // if(character.classList.contains("animate"));
         // {start();   }
-        counter=0;
-    }else{
+        counter=0; 
         
-        counter++;
+
+    }
+    else{
+        if(c)
+       { counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+       }
         
     }
+
+
 }, 10);
